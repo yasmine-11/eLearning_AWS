692b0643e6b99cf75304722fa369df358356ca2f
Yasmine <136965789+yasmine-11@users.noreply.github.com>
2024-09-04 18:47:01 +0400

    Update settings.py

--- elearning_app/settings.py
+++ elearning_app/settings.py
@@ -95,8 +95,12 @@ ASGI_APPLICATION = 'elearning_app.routing.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elearning_aws',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
