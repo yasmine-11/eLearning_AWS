ce6e0bf6f09b1dbea9798860fae8afcbe7df2ef4
Yasmine <136965789+yasmine-11@users.noreply.github.com>
2024-09-05 16:00:05 +0400

    Delete elearning_app/communications/forms.py

--- elearning_app/communications/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import ChatMessage
-
-class ChatMessageForm(forms.ModelForm):
-    class Meta:
-        model = ChatMessage
-        fields = ['message', 'file']
