f061521bd4e0fbe585ad204b152f2e8d86a8b44d
Yasmine <136965789+yasmine-11@users.noreply.github.com>
2024-09-05 15:59:45 +0400

    Update consumers.py

--- elearning_app/communications/consumers.py
+++ elearning_app/communications/consumers.py
@@ -4,6 +4,9 @@ from asgiref.sync import sync_to_async
 from .models import ChatMessage
 from django.contrib.auth import get_user_model
 from courses.models import Course
+import time
+import os
+from django.conf import settings
 
 User = get_user_model()
 
@@ -33,20 +36,33 @@ class CourseChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-        file_url = text_data_json.get('file_url', '')
+        data = json.loads(text_data)
+        message = data['message']
+        file_data = data.get('file', None)
 
-        # Save the message in the database
-        await self.save_message(self.user, message, file_url)
+        file_url = ''
+        if file_data:
+            try:
+                file_name = f"chat_files/{self.user.username}_{int(time.time())}_{file_data['name']}"
+                file_path = os.path.join(settings.MEDIA_ROOT, file_name)
 
-        # Send message to the room group
+                # Save the file
+                with open(file_path, "wb") as f:
+                    f.write(bytearray(file_data['content']))
+
+                file_url = f"{settings.MEDIA_URL}{file_name}"
+            except Exception as e:
+                print(f"Error saving file: {e}")
+
+        await self.save_message(self.user, message, file_url, file_data['name'] if file_data else '')
+
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
                 'file_url': file_url,
+                'file_name': file_data['name'] if file_data else '',
                 'username': self.user.username,
             }
         )
@@ -54,26 +70,24 @@ class CourseChatConsumer(AsyncWebsocketConsumer):
     async def chat_message(self, event):
         message = event['message']
         file_url = event.get('file_url', '')
+        file_name = event.get('file_name', '')
         username = event['username']
 
-        # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
             'file_url': file_url,
+            'file_name': file_name,
             'username': username,
         }))
 
     @sync_to_async
-    def save_message(self, user, message, file_url):
-        # Extract the relative path if full URL is provided
+    def save_message(self, user, message, file_url, file_name):
         relative_path = file_url.replace(settings.MEDIA_URL, '') if file_url else None
 
         ChatMessage.objects.create(
             user=user,
             course=Course.objects.get(id=int(self.room_name.split('_')[1])),
             message=message,
-            file=relative_path if user.user_type == 'teacher' else None  # Only save file if the user is a teacher
+            file=relative_path if user.user_type == 'teacher' else None
         )
-        print(f'Saved message: {chat_message}')
-        return chat_message
-
+        return message
