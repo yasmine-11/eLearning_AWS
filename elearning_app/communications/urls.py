1306ca9a33bbcd54b75b8e18cfc3c473d564a061
Yasmine <136965789+yasmine-11@users.noreply.github.com>
2024-09-05 16:00:16 +0400

    Update urls.py

--- elearning_app/communications/urls.py
+++ elearning_app/communications/urls.py
@@ -3,5 +3,4 @@ from .views import *
 
 urlpatterns = [
     path('notifications/', notifications_view, name='notifications'),
-    path('upload_chat_file/<int:course_id>/', upload_chat_file, name='upload_chat_file'),
 ]
